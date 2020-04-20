9cf5f68fa24488e3a8dba6b66a80ef48eae26fdf
kix2mix2 <kix2mix2@me.com>
2020-04-20 10:13:42 +0200

    improved the projection plotting code... here it could be broken on different data, but i didn't have time to check :(

--- DRflow/dr/plot.py
+++ DRflow/dr/plot.py
@@ -55,7 +55,7 @@ def getImage(path):
     return OffsetImage(plt.imread(path))
 
 
-def plot_projection(input_file, output_folder, idx, labels, paths, color_palette):
+def plot_projection(input_file, output_folder, idx, labels, paths, color_palette, loc=[0,1], fs= (100, 100)):
     print(input_file)
     try:
         df = pd.read_csv(input_file)
@@ -66,40 +66,41 @@ def plot_projection(input_file, output_folder, idx, labels, paths, color_palette
     df = df.loc[idx, :]
     df["paths"] = paths
     df["ll"] = labels
-    fig, ax = plt.subplots(figsize=(100, 100))
-    ax.scatter(df.iloc[:, 0], df.iloc[:, 1])
+    fig, ax = plt.subplots(figsize=fs)
+    ax.scatter(df.loc[:, loc[0]], df.loc[:, loc[1]])
 
     for i, row in df.iterrows():
         try:
-            ab = AnnotationBbox(getImage(row["paths"]), (row[0], row[1]), frameon=True)
+            ab = AnnotationBbox(getImage(row["paths"]), (row[loc[0]], row[loc[1]]), frameon=True)
         except:
             try:
                 ab = AnnotationBbox(
                     getImage(row["paths"].split(".png")[0] + ".jpg"),
-                    (row[0], row[1]),
+                    (row[loc[0]], row[loc[1]]),
                     frameon=True,
                 )
             except:
                 continue
 
-        ab.patch.set_linewidth(4)
-        ab.patch.set_edgecolor(
-            color_palette[
-                int(row["ll"])
-                if row["ll"] < len(color_palette)
-                else len(color_palette) - 1
-            ]
+        ab.patch.set_linewidth(.25)
+        ab.patch.set_edgecolor( 'white'
+            # color_palette[
+            #     int(row["ll"])
+            #     if row["ll"] < len(color_palette)
+            #     else len(color_palette) - 1
+            # ]
         )
-        ab.patch.set_facecolor(
-            color_palette[
-                int(row["ll"])
-                if row["ll"] < len(color_palette)
-                else len(color_palette) - 1
-            ]
+        ab.patch.set_facecolor('white'
+            # color_palette[
+            #     int(row["ll"])
+            #     if row["ll"] < len(color_palette)
+            #     else len(color_palette) - 1
+            # ]
         )
         ax.add_artist(ab)
 
     output_file = output_folder + input_file.split("/")[-1].split(".csv")[0] + ".pdf"
     print('--------')
+    plt.show()
     plt.savefig(output_file)
     plt.close()
